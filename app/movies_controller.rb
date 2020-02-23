487b1cf352b3c01e555ddd543aba41ea72b20101
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-23 19:21:01 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,7 +27,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-   #new_args = args.each {|key, value| self.send(("#{key}="), value)}
+  binding.pry
+   new_args = args.each {|key, value| self.send(("#{key}="), value)}
 
   Movie.create do |m|
     m.title = "Home Alone"
