726e70dda658d4e9f0d7e3a8e5be4b8b68b4c388
Winston Pemberton <winstonpemberton@gmail.com>
2020-02-23 19:27:22 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
   Movie.create do |m|
     binding.pry
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = args.values[0]
+    m.release_date = args.values[1]
   end
 end
 
